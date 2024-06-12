edbc666c2fce6e3b340549b2748fee33547570c8
JustinBale55 <justinbale55@hotmail.com>
2024-06-12 16:16:11 -0600

    Formatting UI
    Added most of what I think I'll need for UI elements. Everything will need to be hooked up through code now.

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    /*  === NOTES ===
+     
+    Combo Spaces
+    8 total spaces
+    4 images for each direction to input
+    1 image that will have 0 Alpha, used in place where there is no input picture
+
+
+
+
+
+
+
+
+
+     */ 
+
+}
